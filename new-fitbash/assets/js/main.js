4e9722606a2fe291d2e75c6324886af6ff5b0115
Sajjad <sajjad.javazi.01@gmail.com>
2023-09-24 16:48:37 +0330

    footer added

--- new-fitbash/assets/js/main.js
+++ new-fitbash/assets/js/main.js
@@ -48,6 +48,61 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form'),
+    calculateCm = document.getElementById('calculate-cm'),
+    calculateKg = document.getElementById('calculate-kg'),
+    calculateMessage = document.getElementById('calculate-message')
 
 
+const calculateBmi = (e) => {
+    e.preventDefault()
+
+    // Check if the fields have a value
+    if (calculateCm.value === '' || calculateKg.value === '') {
+        // Add and remove color
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        // Show message
+        calculateMessage.textContent = 'قد و وزن خود را وارد کنید👨🏻‍💻' 
+
+        // Remove message three seconds
+        setTimeout(()=> {
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else {
+        // BMI Formula
+        const cm = calculateCm.value / 100,
+            kg = calculateKg.value,
+            bmi = Math.round(kg / (cm * cm))
+
+        // Show your health status
+        if(bmi < 18.5){
+            // Add color and display message
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و شما لاغر هستید 😔`  
+        } else if (bmi < 25){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و بدن متناسبی دارید 🥳`
+        } else if(bmi < 30) {
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و اضافه وزن دارید 😕`
+        } else(bmi < 40)
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و چاقی زیادی دارید 😕`
+        
+
+        // To clear the input field
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        // Remove message four seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 4000)
+    }
+}  
+
+
+calculateForm.addEventListener('submit', calculateBmi)
 /*=============== EMAIL JS ===============*/
