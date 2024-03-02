72d9073471f07501e2140e67569111ab7d0753d3
Sajjad <sajjad.javazi.01@gmail.com>
2024-03-02 20:33:32 +0330

    gender selected

--- new-fitbash/assets/js/main.js
+++ new-fitbash/assets/js/main.js
@@ -30,41 +30,41 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
 }
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -77,17 +77,17 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data , .footer__container, .footer__group`)
-sr.reveal(`.home__img`, {delay: 700, origin: 'bottom'})
-sr.reveal(`.logos__img, .program__card , .pricing__card`, {interval: 100})
-sr.reveal(`.choose__img , .calculate__content`, {origin: 'right'})
-sr.reveal(`.choose__content , .calculate__img`, {origin: 'left'})
+sr.reveal(`.home__img`, { delay: 700, origin: 'bottom' })
+sr.reveal(`.logos__img, .program__card , .pricing__card`, { interval: 100 })
+sr.reveal(`.choose__img , .calculate__content`, { origin: 'right' })
+sr.reveal(`.choose__content , .calculate__img`, { origin: 'left' })
 
 /*=============== CALCULATE JS ===============*/
 const calculateForm = document.getElementById('calculate-form'),
     calculateCm = document.getElementById('calculate-cm'),
     calculateKg = document.getElementById('calculate-kg'),
-    calculateMessage = document.getElementById('calculate-message')
-
+    calculateMessage = document.getElementById('calculate-message'),
+    selectedGender = document.querySelector('.gender:checked').value;
 
 const calculateBmi = (e) => {
     e.preventDefault()
@@ -99,10 +99,10 @@ const calculateBmi = (e) => {
         calculateMessage.classList.add('color-red')
 
         // Show message
-        calculateMessage.textContent = 'قد و وزن خود را وارد کنید👨🏻‍💻' 
+        calculateMessage.textContent = 'قد و وزن خود را وارد کنید👨🏻‍💻 In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.'
 
         // Remove message three seconds
-        setTimeout(()=> {
+        setTimeout(() => {
             calculateMessage.textContent = ''
         }, 3000)
     } else {
@@ -112,17 +112,17 @@ const calculateBmi = (e) => {
             bmi = Math.round(kg / (cm * cm))
 
         // Show your health status
-        if(bmi < 18.5){
+        if (bmi < 18.5) {
             // Add color and display message
             calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و شما لاغر هستید 😔`  
-        } else if (bmi < 25){
+            calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و شما لاغر هستید 😔`
+        } else if (bmi < 25) {
             calculateMessage.classList.add('color-green')
             calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و بدن متناسبی دارید 🥳`
-        } else if(bmi < 30) {
+        } else if (bmi < 30) {
             calculateMessage.classList.add('color-green')
             calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و اضافه وزن دارید 😕`
-        } else if (bmi < 40){
+        } else if (bmi < 40) {
             calculateMessage.classList.add('color-green')
             calculateMessage.textContent = `بی‌ام‌ای‌تون ${bmi} هست و چاقی زیادی دارید 😕`
         } else {
@@ -135,11 +135,11 @@ const calculateBmi = (e) => {
         calculateKg.value = ''
 
         // Remove message four seconds
-        setTimeout(() =>{
+        setTimeout(() => {
             calculateMessage.textContent = ''
         }, 4000)
     }
-}  
+}
 
 
 calculateForm.addEventListener('submit', calculateBmi)
@@ -147,8 +147,8 @@ calculateForm.addEventListener('submit', calculateBmi)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message'),
-      contactUser = document.getElementById('contact-user') 
+    contactMessage = document.getElementById('contact-message'),
+    contactUser = document.getElementById('contact-user')
 
 const sendEmail = (e) => {
     e.preventDefault()
@@ -163,29 +163,29 @@ const sendEmail = (e) => {
         contactMessage.textContent = 'باید ایمیل خود را وارد کنید 👆🏻'
 
         // Remove message three seconds
-        setTimeout(()=> {
+        setTimeout(() => {
             contactMessage.textContent = ''
         }, 3000)
     } else {
         // serviceID - templateID - #form - publicKey
-        emailjs.sendForm('service_m10nae3' , 'template_ew0ebbn' , '#contact-form' , 'd9_hwYXOCrCF5wDoF')
+        emailjs.sendForm('service_m10nae3', 'template_ew0ebbn', '#contact-form', 'd9_hwYXOCrCF5wDoF')
             .then(() => {
                 // Show message and add color
                 contactMessage.classList.add('color-green')
                 contactMessage.textContent = 'با موفقیت انجام شد 💪'
 
                 // Remove message after three seconds
-                setTimeout(()=>{
+                setTimeout(() => {
                     contactMessage.textContent = ''
                 }, 3000)
-            }, (error) =>{
+            }, (error) => {
                 // Mail sending error
-                alert('اوپس! انگار یه مشکلی پیش اومده....' , error)
+                alert('اوپس! انگار یه مشکلی پیش اومده....', error)
             })
 
-            // To clear the input field
-            contactUser.value = ''
+        // To clear the input field
+        contactUser.value = ''
     }
-}  
+}
 
 contactForm.addEventListener('submit', sendEmail)
